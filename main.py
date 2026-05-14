7e42f1b8fc1398d4b114f79af1c483c9e4804cb1
Kacts <juanda2004155@gmail.com>
2026-05-14 17:05:05 -0500

    fix MongoDB credentials and collection name

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
 from datetime import datetime
@@ -18,10 +18,10 @@ app.add_middleware(
 # TODO: conectarse al cluster Admonsis  
 # client = MongoClient("mongodb://<usuario>:<contraseña>@157.253.236.88:8087")
 
-client = MongoClient("mongodb://ISIS2304H31202610:nTC8oeLlYokj@157.253.236.88:8087")
-# TODO: conectarse a la base de datos Admonsis  
+client = MongoClient("mongodb://ISIS2304H32202610:IAeFJw2NKaXC@157.253.236.88:8087")
+# TODO: conectarse a la base de datos Admonsis
 # db = client["ISIS*******"]
-db = client["ISIS2304H31202610"]
+db = client["ISIS2304H32202610"]
 
 
 @app.get("/")
@@ -30,19 +30,24 @@ def inicio():
 
 @app.get('/bares/{bar_id}/comentarios')
 def get_comentarios(bar_id: int):
-    comentarios = list(db["comentarios"].find({"bar_id":bar_id},{"_id":0}))
+    comentarios = list(db["comentarios_bares"].find({"bar_id":bar_id},{"_id":0}))
     return comentarios
 
 @app.post('/bares/{bar_id}/comentarios')
-def post_comentario(bar_id: int, datos: dict):
+def post_comentario(bar_id: int, datos: dict = Body(default={})):
     datos['bar_id'] = bar_id
     datos['fecha']  = datetime.now().isoformat()
-    # TODO: completar
+    db["comentarios_bares"].insert_one(datos)
     return {'mensaje': 'Comentario guardado'}
 
-# TODO: implementar GET /bares/{bar_id}/eventos
-# Debe retornar todos los eventos del bar desde la colección 'eventos'
+@app.get('/bares/{bar_id}/eventos')
+def get_eventos(bar_id: int):
+    eventos = list(db["eventos"].find({"bar_id": bar_id}, {"_id": 0}))
+    return eventos
 
-# TODO: implementar POST /bares/{bar_id}/eventos  
-# Debe insertar el evento en la colección 'eventos'
-# Recuerde agregar bar_id y fecha_creacion al documento antes de insertar
+@app.post('/bares/{bar_id}/eventos')
+def post_evento(bar_id: int, datos: dict = Body(default={})):
+    datos['bar_id'] = bar_id
+    datos['fecha_creacion'] = datetime.now().isoformat()
+    db["eventos"].insert_one(datos)
+    return {'mensaje': 'Evento guardado'}
